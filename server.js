3b3013ed25c8469da518571acb4028708e48bfd5
Phong <phongdoan.dev@gmail.com>
2019-12-24 12:53:57 -0800

    craigslist test - success

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+require('dotenv').config();
+const PORT = process.env.PORT;
+const express = require('express');
+const superagent = require('superagent');
+const app = express();
+const pg = require('pg');
+const ejs = require('ejs');
+const methodoverride = require('method-override');
+
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodoverride('_method'));
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', error => console.error(error));
+client.connect();
+
+
+var
+  craigslist = require('node-craigslist'),
+  clientCL = new craigslist.Client({
+    city: 'seattle'
+  }),
+  options = {
+    category: 'cta'
+  };
+
+clientCL
+  .search(options, 'bmw')
+  .then((listings) => listings.forEach(listing => clientCL.details(listing).then(detail => console.log(detail))))
+  .catch((err) => {
+    console.error(err);
+  });
+
+
+app.listen(PORT, () => console.log(`App is running on ${PORT}`));
