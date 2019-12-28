57cd5a6a0f3a0aa3fccfb8f5a01f2a6ac4f453a2
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-12-27 17:17:51 -0800

    added MarketCheck

--- server.js
+++ server.js
@@ -35,5 +35,18 @@ clientCL
     console.error(err);
   });
 
+var settings = {
+  'url': 'http://api.marketcheck.com/v1/search?api_key={{MARKETCHECK_API}}&year=2014&make=ford&latitude=34.05&longitude=-118.24&radius=100&car_type=used&start=0&rows=2&seller_type=dealer',
+  'method': 'GET',
+  'timeout': 0,
+  'headers': {
+    'Host': 'marketcheck-prod.apigee.net'
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
 
 app.listen(PORT, () => console.log(`App is running on ${PORT}`));
