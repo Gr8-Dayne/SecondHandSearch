4a390e1245a964e0a2d1106bfbd91f0b8459c362
Jitendra <47794527+JBajracharya@users.noreply.github.com>
2019-12-28 18:20:52 -0800

    uncommented api codes

--- server.js
+++ server.js
@@ -51,34 +51,34 @@ function deleteCar(req, res) {
 }
 
 
-// var
-//   craigslist = require('node-craigslist'),
-//   clientCL = new craigslist.Client({
-//     city: 'seattle'
-//   }),
-//   options = {
-//     category: 'cta'
-//   };
+var
+  craigslist = require('node-craigslist'),
+  clientCL = new craigslist.Client({
+    city: 'seattle'
+  }),
+  options = {
+    category: 'cta'
+  };
 
-// clientCL
-//   .search(options, 'bmw')
-//   .then((listings) => listings.forEach(listing => clientCL.details(listing).then(detail => console.log(detail))))
-//   .catch((err) => {
-//     console.error(err);
-//   });
+clientCL
+  .search(options, 'bmw')
+  .then((listings) => listings.forEach(listing => clientCL.details(listing).then(detail => console.log(detail))))
+  .catch((err) => {
+    console.error(err);
+  });
 
-// var settings = {
-//   'url': 'http://api.marketcheck.com/v1/search?api_key={{MARKETCHECK_API}}&year=2014&make=ford&latitude=34.05&longitude=-118.24&radius=100&car_type=used&start=0&rows=2&seller_type=dealer',
-//   'method': 'GET',
-//   'timeout': 0,
-//   'headers': {
-//     'Host': 'marketcheck-prod.apigee.net'
-//   },
-// };
+var settings = {
+  'url': 'http://api.marketcheck.com/v1/search?api_key={{MARKETCHECK_API}}&year=2014&make=ford&latitude=34.05&longitude=-118.24&radius=100&car_type=used&start=0&rows=2&seller_type=dealer',
+  'method': 'GET',
+  'timeout': 0,
+  'headers': {
+    'Host': 'marketcheck-prod.apigee.net'
+  },
+};
 
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
 
 
 app.listen(PORT, () => console.log(`App is running on ${PORT}`));
