6aa147072f01b3d3a599fdaa9e99b01c3dc6be8c
Phong <phongdoan.dev@gmail.com>
2020-01-01 19:34:12 -0800

    error handler on most functions

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ function getSearchPage(req, res) {
 async function postSearchResults(req, res) {
   vehicleResultsArray = [];
   const clientCL = new craigslist.Client({
-    city: req.body.location
-  }),
+      city: req.body.location
+    }),
     options = {
       category: 'cta',
     };
@@ -64,13 +64,13 @@ async function postSearchResults(req, res) {
         let vehicleResult = new Vehicles(listingInfo, listings[i].price);
         vehicleResultsArray.push(vehicleResult);
       } catch (error) {
-        console.log(error)
+        errorHandler(error, res);
       }
     }
     console.log('vehicleResultsArray :', vehicleResultsArray);
     res.render('searchResult.ejs', { vehicles: vehicleResultsArray });
   } catch (error) {
-    console.log(error)
+    errorHandler(error, res);
   }
 }
 
@@ -87,18 +87,25 @@ function saveToDatabase(req, res) {
   const instruction = `INSERT INTO vehicles(title, lat, long, image_URL, CL_URL)
   VALUES ($1, $2, $3, $4, $5)`;
   let values = [req.body.title, req.body.lat, req.body.long, req.body.image, req.body.url];
-  client.query(instruction, values);
-  res.status(204).send();
+  try {
+    client.query(instruction, values);
+    res.status(204).send();
+  }
+  catch (error) {
+    errorHandler(error, res);
+  }
 }
 
 function displaySavedCars(req, res) {
   client.query(`SELECT * FROM vehicles;`).then(savedCars => {
-    res.render('savedCars.ejs', {vehicles: savedCars.rows});
+    res.render('savedCars.ejs', { vehicles: savedCars.rows });
+  }).catch(error => {
+    errorHandler(error, res);
   })
 }
 
 function errorHandler(error, response) {
-  response.render('pages/error', {
+  response.render('error', {
     message: error
   });
 }
