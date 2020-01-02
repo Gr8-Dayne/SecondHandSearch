b1cabd7e5a78172126a7553914743e9b1fb1e741
Phong <phongdoan.dev@gmail.com>
2020-01-02 09:50:35 -0800

    changed schema for marketcheck

--- data/schema.sql
+++ data/schema.sql
@@ -13,6 +13,7 @@ CREATE TABLE IF NOT EXISTS vehicles (
   long VARCHAR(255),
   image_URL VARCHAR(255),
   CL_URL VARCHAR(255),
+  market_value NUMERIC,
   userID INT,
   FOREIGN KEY (userID) REFERENCES users(ID)
 );
@@ -23,7 +24,15 @@ VALUES
   ('test_user1');
 
 INSERT INTO
-  vehicles (title, lat, long, image_URL, CL_URL, userID)
+  vehicles (
+    title,
+    lat,
+    long,
+    image_URL,
+    CL_URL,
+    market_value,
+    userID
+  )
 VALUES
   (
     'BMW M3',
@@ -31,6 +40,7 @@ VALUES
     '37.6173',
     'https://cdn.bringatrailer.com/wp-content/uploads/2018/08/2003_bmw_m3_coupe_15360831063b2253e7Screen-Shot-2018-09-04-at-10.43.10-AM-940x636.png',
     'https://seattle.craigslist.org/search/sss?query=BMW+M3&excats=122-41&sort=rel',
+    '35474732',
     (
       SELECT
         ID
@@ -42,7 +52,15 @@ VALUES
   );
 
 INSERT INTO
-  vehicles (title, lat, long, image_URL, CL_URL, userID)
+  vehicles (
+    title,
+    lat,
+    long,
+    image_URL,
+    CL_URL,
+    market_value,
+    userID
+  )
 VALUES
   (
     'Lada',
@@ -50,6 +68,7 @@ VALUES
     '30.5234',
     'https://i.pinimg.com/originals/2b/7d/bc/2b7dbc791cbda8a670e80abaeffea9ea.jpg',
     'https://seattle.craigslist.org/search/sss?query=lada&sort=rel',
+    '234561',
     (
       SELECT
         ID
@@ -61,7 +80,15 @@ VALUES
   );
 
 INSERT INTO
-  vehicles (title, lat, long, image_URL, CL_URL, userID)
+  vehicles (
+    title,
+    lat,
+    long,
+    image_URL,
+    CL_URL,
+    market_value,
+    userID
+  )
 VALUES
   (
     'BMW M4',
@@ -69,6 +96,7 @@ VALUES
     '122.2348',
     'https://i.pinimg.com/originals/19/a8/6c/19a86c8abef148ebe0bfad0fb49f57be.jpg',
     'https://seattle.craigslist.org/search/sss?query=BMW%20M4&sort=rel',
+    '1234567',
     (
       SELECT
         ID
@@ -85,7 +113,15 @@ VALUES
   ('test_user2');
 
 INSERT INTO
-  vehicles (title, lat, long, image_URL, CL_URL, userID)
+  vehicles (
+    title,
+    lat,
+    long,
+    image_URL,
+    CL_URL,
+    market_value,
+    userID
+  )
 VALUES
   (
     'BMW M3',
@@ -93,6 +129,7 @@ VALUES
     '37.6173',
     'https://cdn.bringatrailer.com/wp-content/uploads/2018/08/2003_bmw_m3_coupe_15360831063b2253e7Screen-Shot-2018-09-04-at-10.43.10-AM-940x636.png',
     'https://seattle.craigslist.org/search/sss?query=BMW+M3&excats=122-41&sort=rel',
+    '098765',
     (
       SELECT
         ID
@@ -104,7 +141,15 @@ VALUES
   );
 
 INSERT INTO
-  vehicles (title, lat, long, image_URL, CL_URL, userID)
+  vehicles (
+    title,
+    lat,
+    long,
+    image_URL,
+    CL_URL,
+    market_value,
+    userID
+  )
 VALUES
   (
     'Lada',
@@ -112,6 +157,7 @@ VALUES
     '30.5234',
     'https://i.pinimg.com/originals/2b/7d/bc/2b7dbc791cbda8a670e80abaeffea9ea.jpg',
     'https://seattle.craigslist.org/search/sss?query=lada&sort=rel',
+    '234567',
     (
       SELECT
         ID
@@ -123,7 +169,15 @@ VALUES
   );
 
 INSERT INTO
-  vehicles (title, lat, long, image_URL, CL_URL, userID)
+  vehicles (
+    title,
+    lat,
+    long,
+    image_URL,
+    CL_URL,
+    market_value,
+    userID
+  )
 VALUES
   (
     'BMW M4',
@@ -131,6 +185,7 @@ VALUES
     '122.2348',
     'https://i.pinimg.com/originals/19/a8/6c/19a86c8abef148ebe0bfad0fb49f57be.jpg',
     'https://seattle.craigslist.org/search/sss?query=BMW%20M4&sort=rel',
+    '56789',
     (
       SELECT
         ID
